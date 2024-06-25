027542ccf42c7614db50e03924428f0b2adc4392
Ariel Spencer <arielspencer.tech@gmail.com>
2024-06-25 18:21:48 -0300

    Adicionar metodo para automatizar desconto em todos os livros

--- app/main.js
+++ app/main.js
@@ -1,30 +1,10 @@
-let livros = []
+let livros = [];
 const endPointAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json';
 getBuscarLivrosDaAPI();
-const elementoParaInserirLivros = document.getElementById('livros');
 
 async function getBuscarLivrosDaAPI() {
-    const res = await fetch(endPointAPI)
-    livros = await res.json()
-    console.table(livros);
-    exibirLivrosNaTela(livros);
+    const res = await fetch(endPointAPI);
+    livros = await res.json();
+    let livrosComDesconto = aplicarDesconto(livros);
+    exibirLivrosNaTela(livrosComDesconto);
 }
-
-function exibirLivrosNaTela(listaDeLivros){
-    livros.forEach(livro => {
-        elementoParaInserirLivros.innerHTML += `
-    <div class="livro">
-      <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
-      <h2 class="livro__titulo">
-      ${livro.titulo}
-      </h2>
-      <p class="livro__descricao">${livro.autor}</p>
-      <p class="livro__preco" id="preco">R$${livro.preco}</p>
-      <div class="tags">
-        <span class="tag">${livro.categoria}/span>
-      </div>
-    </div>
-        `
-    })
-}
-
