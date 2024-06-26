0a125c8df7ccaa34738b38374b21030003bc0081
Ariel Spencer <arielspencer.tech@gmail.com>
2024-06-26 19:01:59 -0300

    Mostrar indisponibilidade do livro no campo valor

--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -4,6 +4,7 @@ function exibirLivrosNaTela(listaDeLivros){
     elementoParaInserirLivros.innerHTML = ''
     listaDeLivros.forEach(livro => {
         let disponibilidade = livro.quantidade > 0 ? 'livro__imagens' : 'livro__imagens indisponivel';
+        let valorLivro = livro.quantidade > 0 ? `R$${livro.preco.toFixed(2)}` : 'Livro Indisponível';
         elementoParaInserirLivros.innerHTML += `
     <div class="livro">
       <img class="${disponibilidade}" src="${livro.imagem}" alt="${livro.alt}" />
@@ -11,7 +12,7 @@ function exibirLivrosNaTela(listaDeLivros){
       ${livro.titulo}
       </h2>
       <p class="livro__descricao">${livro.autor}</p>
-      <p class="livro__preco" id="preco">R$${livro.preco.toFixed(2)}</p>
+      <p class="livro__preco" id="preco">${valorLivro}</p>
       <div class="tags">
         <span class="tag">${livro.categoria}</span>
       </div>
