9d10c19602d29c3f2bb2c3f679e6e641a804dbda
Ariel Spencer <arielspencer.tech@gmail.com>
2024-06-26 19:30:23 -0300

    Remover log de filtros

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -6,6 +6,5 @@ function filtrarLivros() {
     const categoria = elementoBtn.value;
 
     let livrosFiltrados = categoria == 'disponivel' ? livros.filter(livro => livro.quantidade > 0) : livros.filter(livro => livro.categoria == categoria);
-    console.log(livrosFiltrados);
     exibirLivrosNaTela(livrosFiltrados);
 }
